7fc9687069cb0acda1ccf64ba33efda892b4ba23
Leandro-Cardoso <leandrolimacardoso@hotmail.com>
2024-05-27 15:15:35 -0300

    Get lines from TXT function

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ from pytube import YouTube
 
 from config import LINKS_FILE, DOWNLOADED_PATH, MAX_RESOLUTION
 
-def main():
-    with open(LINKS_FILE, 'r') as file:
-        links = []
-        for line in file:
-            link = line.strip()
-            links.append(link)
+def get_lines_from_txt(txt_file:str) -> list:
+    with open(txt_file, 'r') as file:
+        lines = []
+        for i in file:
+            line = i.strip()
+            lines.append(line)
         file.close()
+    return lines
+
+def main():
+    links = get_lines_from_txt(LINKS_FILE)
     print(links)
 
 if __name__ == '__main__':
