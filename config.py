8cbb5f1c1472e5c5e87d4965ea49391bfc4fcc03
Leandro-Cardoso <leandrolimacardoso@hotmail.com>
2024-05-27 15:37:47 -0300

    Convert to filename function

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 LINKS_FILE = 'links.txt'
-DOWNLOADED_PATH = 'downloaded/'
 MAX_RESOLUTION = '1080p'
+OUTPUT_PATH = 'downloaded'
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pytube import YouTube
 
-from config import LINKS_FILE, DOWNLOADED_PATH, MAX_RESOLUTION
+from config import LINKS_FILE, MAX_RESOLUTION, OUTPUT_PATH
 
 def get_lines_from_txt(txt_file:str) -> list:
     with open(txt_file, 'r') as file:
@@ -11,9 +11,28 @@ def get_lines_from_txt(txt_file:str) -> list:
         file.close()
     return lines
 
+def convert_to_filename(name:str, format:str) -> str:
+    filename = str(name)
+    if '.' in format:
+        filename += str(format)
+    else:
+        filename += '.' + str(format)
+    filename = filename.lower()
+    filename = filename.replace(' ', '_')
+    return filename
+
 def main():
     links = get_lines_from_txt(LINKS_FILE)
-    print(links)
+    for link in links:
+        try:
+            yt = YouTube(link)
+            stream = yt.streams.filter(resolution = MAX_RESOLUTION).first()
+            title = yt.title
+            format = 'mp4'
+            filename = convert_to_filename(title, format)
+            stream.download(output_path = OUTPUT_PATH, filename = filename)
+        except Exception as e:
+            print(f'Erro: {link} -> {e}')
 
 if __name__ == '__main__':
     main()
